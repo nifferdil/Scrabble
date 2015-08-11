49bb9601debb9e4b83346cfea32f908703a9b46a
Jennifer Morkunas <nifferdil@gmail.com>
2015-08-11 16:22:12 -0700

    Fixed score error

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -34,34 +34,34 @@ public class Scrabble {
 
         //set values to letters
 
-        HashMap<Character, Integer> tiles = new HashMap<>();
+        HashMap<String, Integer> tiles = new HashMap<>();
 
-        tiles.put('A',1);
-        tiles.put('E',1);
-        tiles.put('I',1);
-        tiles.put('O',1);
-        tiles.put('U',1);
-        tiles.put('L',1);
-        tiles.put('N',1);
-        tiles.put('R',1);
-        tiles.put('S',1);
-        tiles.put('T',1);
-        tiles.put('D',2);
-        tiles.put('G',2);
-        tiles.put('B',3);
-        tiles.put('C',3);
-        tiles.put('M',3);
-        tiles.put('P',3);
-        tiles.put('F',4);
-        tiles.put('H',4);
-        tiles.put('V',4);
-        tiles.put('W',4);
-        tiles.put('Y',4);
-        tiles.put('K',5);
-        tiles.put('J',8);
-        tiles.put('X',8);
-        tiles.put('Q',10);
-        tiles.put('Z',10);
+        tiles.put("A",1);
+        tiles.put("E",1);
+        tiles.put("I",1);
+        tiles.put("O",1);
+        tiles.put("U",1);
+        tiles.put("L",1);
+        tiles.put("N",1);
+        tiles.put("R",1);
+        tiles.put("S",1);
+        tiles.put("T",1);
+        tiles.put("D",2);
+        tiles.put("G",2);
+        tiles.put("B",3);
+        tiles.put("C",3);
+        tiles.put("M",3);
+        tiles.put("P",3);
+        tiles.put("F",4);
+        tiles.put("H",4);
+        tiles.put("V",4);
+        tiles.put("W",4);
+        tiles.put("Y",4);
+        tiles.put("K",5);
+        tiles.put("J",8);
+        tiles.put("X",8);
+        tiles.put("Q",10);
+        tiles.put("Z",10);
 
         // break the word up
         String[] letters = word.split("");
@@ -73,8 +73,10 @@ public class Scrabble {
 
         Integer score = 0;
         for(int i=0; i < letters.length ; i++) {
-            String letter = letters[i];
+            String letter = letters[i].toUpperCase();
+            System.out.println(tiles.get(letter));
             Integer letterScore = tiles.get(letter);
+            System.out.println(letterScore);
             score = score + letterScore;
         }
         return score;
